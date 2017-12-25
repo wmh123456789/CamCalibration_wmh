821d8f3cfad83460556f98c9ef7b65b3a02a876a
wmh123456789 <wmh123456789@qq.com>
2017-12-25 17:38:30 +0800

    Gravity retrieval Real accelerate calculate

---
+++ IMULib.py
@@ -0,0 +1,44 @@
+# some IMU ultity
+import numpy as np
+from matplotlib import pylab
+from mpl_toolkits import mplot3d
+
+def loadcsv(CSVFILE):
+    dataset = []
+    # scale = 1
+    for line in open(CSVFILE).readlines():
+        data = line.split(",")
+        if data[0][0].isdigit():
+            dataset.append([float(x) for x in data])
+    return dataset
+
+# Rawdata is a numpy array, raw ACC output
+def GravityFilter(Rawdata, g0 = np.array([0.0,0.0,0.0])):
+    alpha = 0.8  # refer to Android SDK
+    # g = np.array([0.0,0.0,0.0])
+
+    g = alpha*g0 + (1-alpha)*Rawdata
+    Acc = Rawdata - g
+    return  g,Acc
+    pass
+
+def main():
+    CSVFILE = '/media/wmh/TEMP/SlamTestSeq/MySeq/Still1/RobotState.csv'
+    dataset = loadcsv(CSVFILE)
+    ColStart = 10
+    ColEnd = 12
+    gravity = np.array([0.0,0.0,0.0])
+    AccSum = np.array([0.0,0.0,0.0])
+
+
+    for data in dataset:
+        gravity,acc = GravityFilter(np.array(data[ColStart:ColEnd+1]),gravity)
+        AccSum += acc
+        print "g:",gravity,":",np.linalg.norm(gravity),"; Acc:",acc
+
+    print "Sum of Acc:", AccSum
+    pass
+
+
+if __name__ == '__main__':
+    main()
--- Try.py
+++ Try.py
@@ -1,21 +1,32 @@
 import numpy as np
 from itertools import combinations, permutations
 
-a = ["1","2","3","4"]
+a = ["1.0","2.0","3.0","4.0"]
 b = np.array([4,5,6])
 c = np.array([1,2,3])
 cc = 'abcd'
 
 print np.array(b)+np.array(c)
 print np.dot(b[:2],c[:2])
+print 10.1*b
 
-print list(permutations([1, 2, 3], 2))
+print [float(x) for x in a]
+
+# print list(permutations([1, 2, 3], 2))
 # print list(combinations([1, 2, 3], 2))
 # print permutations([1, 2, 3], 2)
 # print combinations([1, 2, 3], 2)
-l = list(combinations([1,2,3,4],2))
-for a,b in combinations([1,2,3,4],2):
-    print a,b
+# l = list(combinations([1,2,3,4],2))
+# for a,b in combinations([1,2,3,4],2):
+#     print a,b
+
+
+
+zz = ['a','3','0.2','12.3f']
+for e in zz:
+    print e,":",e[0].isdigit()
+
+
 
 
 # c = []
